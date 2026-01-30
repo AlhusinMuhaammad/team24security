cbbdece639c934e68dfbf54e49f09cd273f7e2ff
Muhammad Al Husin <m.alhusin.education@gmail.com>
2026-01-30 15:02:48 +0100

    Refactor middleware to always allow requests
    Simplified middleware function to always allow requests.

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const allowedExact = new Set(['/', '/favicon.ico', '/robots.txt', '/sitemap.xml'])
-const allowedPrefixes = ['/_next', '/_vercel', '/images', '/api']
-
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
-
-  if (allowedExact.has(pathname) || allowedPrefixes.some((prefix) => pathname.startsWith(prefix))) {
-    return NextResponse.next()
-  }
-
-  return NextResponse.redirect(new URL('/', request.url))
-}
-
-export const config = {
-  matcher: '/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|images|api).*)',
+export function middleware(_request: NextRequest) {
+  return NextResponse.next()
 }
 
